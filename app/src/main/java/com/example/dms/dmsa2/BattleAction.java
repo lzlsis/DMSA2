12c6bacb4ad282770317335fca9e7b31710a41e4
jaimesBooth <jaimesbooth@Jaimess-MacBook-Pro-2.local>
2015-05-08 11:26:54 +1200

    Commented code to try and gain better understanding. Removed unused code for easier reading.

--- app/src/main/java/com/example/dms/dmsa2/BattleAction.java
+++ app/src/main/java/com/example/dms/dmsa2/BattleAction.java
@@ -10,7 +10,6 @@ import android.nfc.NfcAdapter.CreateNdefMessageCallback;
 import android.nfc.NfcEvent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,11 +19,15 @@ import java.util.Random;
 import java.util.StringTokenizer;
 
 /**
+ * Represents a Battle action object which is transmitted to the opponent
+ * via NFC.
+ *
  * http://www.wikihow.com/Make-a-Rock%2C-Paper%2C-Scissors-Game-in-Java
  * https://developer.android.com/guide/topics/connectivity/nfc/nfc.html
  *
  * @author jaimesbooth 20150504
  * @modified jaimesbooth 20150505 Imported to Android project from Java CLI
+ * @modified andy 20150507 added Bluetooth, modified NFC functionality and game play
  */
 public class BattleAction extends Activity implements CreateNdefMessageCallback{
 
@@ -35,21 +38,30 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
     private String myMove;
     private String underAttack;
 
+    /**
+     * Called when this Activity is started.
+     * @param savedInstanceState If the activity is being re-initialized after previously
+     *                           being shut down then this Bundle contains the data it most
+     *                           recently supplied in onSaveInstanceState(Bundle).
+     *                           Note: Otherwise it is null.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle_action);
         // Associate the player's text field to the variable text view
         textView = (TextView) findViewById(R.id.attackText);
+        // Set the player's ID based on the registered Bluetooth name
         myID = BluetoothAdapter.getDefaultAdapter().getName();
+        // Initialise player's move
         myMove = "";
+        // Initialise underAttack
         underAttack = "";
-        // game.startGame();
 
         // Set up NFC when battlefield is started
-        //TextView textView = (TextView) findViewById(R.id.textView1);
         // Check for available NFC Adapter
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        // Close Activity if NFC is not available
         if (mNfcAdapter == null) {
             Toast.makeText(this, "NFC is not available", Toast.LENGTH_LONG)
                     .show();
@@ -66,24 +78,43 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
     }
 
     /**
-     * Creates an NdefMessage that contains the NdefRecords (attack and player ID)
-     * that are to be pushed onto the other device.
+     * Creates a NdefMessage that contains the NdefRecords (attack and player ID)
+     * that are to be pushed onto the other device. Called when this device is
+     * in range of another device that might support NDEF push.
+     * It allows the application to create the NDEF message only when it is required.
+     * @param event NfcEvent with the nfcAdapter field set
+     * @return The NDEF message with embedded player id and player move to push, or null to not provide a message
      */
     @Override
     public NdefMessage createNdefMessage(NfcEvent event) {
-        //String playerId = "0001";
         // Get the player's attack move
+        // Generate a random move if the player's move has not been set.
         if (myMove.length() == 0) {
             myMove = randomMove();
 //            textView.setText("You chose " + myMove);
         }
+        // Embed player id and player move into the NDEF message
         String textMessage = (myID + "_" + myMove);
         NdefMessage msg = new NdefMessage(new NdefRecord[]{createMimeRecord(
                 "text/plain", textMessage.getBytes())});
         return msg;
     }
 
+    /**
+     * Creates a custom MIME type encapsulated in an NDEF record
+     *
+     * @param mimeType The name of the MIME type to encapsulate
+     */
+    public NdefRecord createMimeRecord(String mimeType, byte[] payload) {
+        byte[] mimeBytes = mimeType.getBytes(Charset.forName("US-ASCII"));
+        NdefRecord mimeRecord = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,
+                mimeBytes, new byte[0], payload);
+        return mimeRecord;
+    }
 
+    /**
+     * Called when the activity will start interacting with the user.
+     */
     @Override
     public void onResume() {
         super.onResume();
@@ -93,6 +124,17 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
         }
     }
 
+    /**
+     * When the activity is re-launched while at the top of the activity stack
+     * instead of a new instance of the activity being started, onNewIntent()
+     * will be called on the existing instance with the Intent that was
+     * used to re-launch it.
+     *
+     * An activity will always be paused before receiving a new intent,
+     * so you can count on onResume() being called after this method.
+     *
+     * @param intent The new intent that was started for the activity.
+     */
     @Override
     public void onNewIntent(Intent intent) {
         // onResume gets called after this to handle the intent
@@ -100,23 +142,28 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
     }
 
     /**
-     * Parses the NDEF Message from the intent and prints to the opponents
-     * TextView
+     * Parses the NDEF Message from the intent and informs player
+     * of the opponents attack
      */
     void processIntent(Intent intent) {
-        textView = (TextView) findViewById(R.id.attackText);
         Parcelable[] rawMsgs = intent
                 .getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
         // only one message sent during the beam
         NdefMessage msg = (NdefMessage) rawMsgs[0];
+
+        // Populate the underAttack String with the opponents id and attack move
         // record 0 contains the MIME type, record 1 is the AAR, if present
         underAttack = new String(msg.getRecords()[0].getPayload());
-//        textView.setText(underAttack + "\nYou chose " + myMove);
 
+        // Notify player of attack
         textView.setText("You are under attack, \nselect your move to react");
 
     }
 
+    /**
+     * Generates a random attack move
+     * @return A random attack move
+     */
     private String randomMove() {
         Random random = new Random();
         int choice = random.nextInt(3);
@@ -132,6 +179,13 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
         }
     }
 
+    /**
+     * Computes the result (hit:win, counter:lose, missed:tie) of comparing
+     * a player's move with the opponents move.
+     * @param moveA Player's move
+     * @param moveB Opponents move
+     * @return The result of the comparison of attacks
+     */
     public String computeResult(String moveA, String moveB) {
         // Tie
         if (moveA.equalsIgnoreCase(moveB)) {
@@ -140,6 +194,8 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
 
         switch (moveA) {
             case "ROCK":
+                // Ternary operator. If (testCondition is true), assign the value before the ":"
+                // to result; otherwise, assign the value after ":" to result."
                 return (moveB.equalsIgnoreCase("SCISSORS") ? "hit" : "counter");
             case "PAPER":
                 return (moveB.equalsIgnoreCase("ROCK") ? "hit" : "counter");
@@ -151,6 +207,10 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
         return "ERROR";
     }
 
+    /**
+     * Called when the activity has detected the user's press of the back key.
+     * Finishes the current activity.
+     */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
@@ -159,65 +219,10 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
     }
 
     /**
-     * Creates a custom MIME type encapsulated in an NDEF record
-     *
-     * @param mimeType
+     * Called when a view has been clicked.
+     * Handles click events (to the buttons) in the view.
+     * @param v The view that was clicked
      */
-    public NdefRecord createMimeRecord(String mimeType, byte[] payload) {
-        byte[] mimeBytes = mimeType.getBytes(Charset.forName("US-ASCII"));
-        NdefRecord mimeRecord = new NdefRecord(NdefRecord.TNF_MIME_MEDIA,
-                mimeBytes, new byte[0], payload);
-        return mimeRecord;
-    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.battlefield, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    /**
-//     * Called when the user touches the Rock button
-//     */
-//    public void setAttackRock(View view) {
-//        // Set attack to rock in response to button click
-//        myMove = "ROCK";
-//        textView.setText("==You chose " + myMove);
-//    }
-//
-//    /**
-//     * Called when the user touches the Paper button
-//     * //
-//     */
-//    public void setAttackPaper(View view) {
-//        // Set attack to paper in response to button click
-//        myMove = "PAPER";
-//        textView.setText("==You chose Paper");
-//    }
-//
-//    /**
-//     * Called when the user touches the Scissors button
-//     */
-//    public void setAttackScissors(View view) {
-//        // Set attack to scissors in response to button click
-//        myMove = "SCISSORS";
-//        textView.setText("==You chose " + myMove);
-//    }
-
-//    @Override
     public void onClick(View v) {
         if (v == findViewById(R.id.bRock))
             myMove = "ROCK";
@@ -225,158 +230,49 @@ public class BattleAction extends Activity implements CreateNdefMessageCallback{
             myMove = "PAPER";
         else if (v == findViewById(R.id.bScisssors))
             myMove = "SCISSORS";
+        // Change the text view to inform player of their chosen move
         textView.setText("==You chose " + myMove);
+
+        // Check if the player is under attack
         if (underAttack.length() > 0) {
+            // If under attack request a counter attack
             takeReaction();
         }
     }
 
+    /**
+     * Called when a player is under attack.
+     * Extracts the opponents id and opponents attack move from
+     * the underAttack string.
+     */
     private void takeReaction() {
+        // Extract the opponent's id and attack move from
+        // the underAttack string.
         StringTokenizer st = new StringTokenizer(underAttack, "_");
         String enemyID = st.nextToken();
         String enemyMove = st.nextToken();
 
+        // Evaluate the player's and opponents move
         String result = computeResult(enemyMove, myMove);
+
+        // Inform player of result of battle
         String resultMessage = enemyID + "_" + myID + "_" + result;
         textView.setText(resultMessage);
+
+
+        // Depending on the computed battle result
+        // If there was an error cancel result
         if (result.equalsIgnoreCase("ERROR")) {
             setResult(RESULT_CANCELED);
+        // Otherwise, return the result message to the Battlefield Activity
         } else {
             Intent i = new Intent(this, BattlefieldActivity.class);
             i.putExtra("resultMessage", resultMessage);
             setResult(RESULT_OK, i);
         }
+        // Call finish() when your activity is done and should be closed.
+        // The ActivityResult is propagated back to whoever launched you via onActivityResult().
         finish();
 
     }
-
-//    public BattleAction() {
-//        player = new Player();
-//        //computer = new Computer();
-//        //userScore = 0;
-//        //computerScore = 0;
-//        //numberOfGames = 0;
-//        //userMove = null;
-//
-//    }
-
-//	public void startGame() {
-//		//System.out.println("ROCK, PAPER, SCISSORS!");
-//
-//		//Move userMove = user.getMove();
-//		//Move computerMove = computer.getMove();
-//		//System.out.println("\nYou played " + userMove + ".");
-//		//System.out.println("Computer played " + computerMove + ".\n");
-//
-//		int compareMoves = userMove.compareMoves(computerMove);
-//		switch (compareMoves) {
-//		case 0: // Tie
-//			System.out.println("Tie!");
-//			break;
-//		case 1: // User wins
-//			System.out.println(userMove + " beats " + computerMove
-//					+ ". You won!");
-//			userScore++;
-//			break;
-//		case -1: // Computer wins
-//			System.out.println(computerMove + " beats " + userMove
-//					+ ". You lost.");
-//			computerScore++;
-//			break;
-//		}
-//		numberOfGames++;
-//
-//		if (user.playAgain()) {
-//			System.out.println();
-//			startGame();
-//		}
-//
-//	}
-//
-//    private enum Move {
-//
-//        ROCK, PAPER, SCISSORS;
-//
-//        /**
-//         * Compares this move with another move to determining a tie, a win, or
-//         * a loss.
-//         *
-//         * @param otherMove move to compare to
-//         * @return 1 if this move beats the other move, -1 if this move loses to
-//         * the other move, 0 if these moves tie
-//         */
-//        public int compareMoves(Move otherMove) {
-//            // Tie
-//            if (this == otherMove) {
-//                return 0;
-//            }
-//
-//            switch (this) {
-//                case ROCK:
-//                    return (otherMove == SCISSORS ? 1 : -1);
-//                case PAPER:
-//                    return (otherMove == ROCK ? 1 : -1);
-//                case SCISSORS:
-//                    return (otherMove == PAPER ? 1 : -1);
-//            }
-//
-//            // Should never reach here
-//            return 0;
-//        }
-//    }
-//
-//    /**
-//     * Represents a player of the game
-//     */
-//    private class Player {
-//
-//        /**
-//         * Creates a player object
-//         */
-//        public Player() {
-//            userMove = null;
-//        }
-//
-//        /**
-//         * Sets the player's attack move.
-//         *
-//         * @return The user's move
-//         */
-//        public void setMove(Move move) {
-//            // Get the user input
-//            userMove = move;
-//        }
-//
-//        /**
-//         * Gets the player's attack move.
-//         *
-//         * @return The user's move
-//         */
-//        public Move getMove() {
-//
-//            // Get the user input
-//            return userMove;
-//
-//        }
-//    }
-
-//	/**
-//	 * Represents a second player which is able to
-//	 * submit a random attack.
-//	 *
-//	 */
-//	private class Computer {
-//
-//		/**
-//		 * Generates a random attack from a second player.
-//		 * @return
-//		 */
-//		public Move getMove() {
-//			Move[] moves = Move.values();
-//			Random random = new Random();
-//			int index = random.nextInt(moves.length);
-//			return moves[index];
-//		}
-//	}
-
 }
